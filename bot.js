58bc544a26f6d1896b66b591d115012d120f02f4
Luke <ls748@kent.ac.uk>
2021-02-16 13:43:34 +0000

    I have updated the tea and biscuit code.

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ function answerQuery(query) {
   query = query.toLowerCase();
   if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1)
     return "I do not care too much about weather, I'm locked inside a data center.";
+  if (query.indexOf("tea") != -1 || query.indexOf("biscuit") != -1)
+	return "I would love some tea and biscuits.";
   return "Sorry Dave, I cannot do that."
 }
 
