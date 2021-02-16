30e76c8937cb1d3ed6f1aea0057670301e71715d
Harry Jones <harryalexjones@yahoo.co.uk>
2021-02-16 13:37:35 +0000

    Added football conversation

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ function answerQuery(query) {
   query = query.toLowerCase();
   if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1)
     return "I do not care too much about weather, I'm locked inside a data center.";
+  if (query.indexOf("football") != -1 || query.indexOf("sport") != -1 || query.indexOf("goal") != -1)
+    return "Football does not interest me. Instead, let's focus on the mission Dave";
   return "Sorry Dave, I cannot do that."
 }
 
